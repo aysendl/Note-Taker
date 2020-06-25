618239d01d357fd37af6aa9aa63a1f3c90feffc7
Ayse <aysendl@gmail.com>
2020-06-24 22:52:14 -0700

    Fixing port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT =3001; 
+var PORT = process.env.PORT || 3001; 
 const app = express();
 const htmlRoutes = require ("./routes/htmlRoutes");
 const apiRoutes = require ("./routes/apiRoutes");
